9c0695132e776cd0ebdbea7b10d66a3c0257ad52
Katherine <katherinesteiner2019@u.northwestern.edu>
2019-01-13 20:39:56 -0600

    Add files via upload

---
+++ hw1/student_code_1.h
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////////////////////////
+// You need to 
+//    1. Read Problem 2 in homework assignmnet #1. 
+//    2. Implement function GetStudentName.
+//    3. Implement function CountNUOccurrences.
+//    4. Compile your code as explained in the PDF file.
+//    5. Run the executable.
+//    6. Test and debug your code.
+//    7. Submit your code and results via Canvas.
+///////////////////////////////////////////////////////////////////////////////
+
+//you can include standard C++ libraries here
+
+//required libraries
+#include <string>
+
+// This function should return your name.
+// The name should match your name in Canvas
+void GetStudentName(std::string& your_name)
+{
+   //replace Firstname and Lastname below with you first name and last name
+   your_name.assign("Firstname Lastname");
+}
+
+//This is the function you need to implement.
+int CountNUOccurrences(const std::string& message)
+{
+   
+   //your code goes here
+
+   return  -1; /* number of occurrences */
+}
