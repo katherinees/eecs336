ea4bbd4ba4e222347c9640d13297cbad9a6adbe7
katherinees <katherinesteiner2019@u.northwestern.edu>
2019-01-19 15:21:09 -0600

    adding files for hw2

---
+++ hw2/student_code_2.h
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////////////////////////
+// You need to 
+//    1. Read Problem 2 in Homework Assignment #2. 
+//    2. Implement function GetStudentName.
+//    3. Implement function FindMaxSchedule.
+//    4. Compile your code as explained in the PDF file.
+//    5. Run the executable.
+//    6. Test and debug your code.
+//    7. Submit your code ("student_code_2.h") and results ("solution_2.dat") 
+//       via Canvas.
+///////////////////////////////////////////////////////////////////////////////
+
+//required libraries
+#include <string>
+#include <vector> 
+
+//you can include standard C++ libraries here
+
+
+// This function should return your name.
+// The name should match your name in Canvas
+
+void GetStudentName(std::string& your_name)
+{
+   //replace the placeholders "Firstname" and "Lastname"
+   //with you first name and last name 
+   your_name.assign("Firstname Lastame");
+}
+
+
+//you can use one of the following signatures
+
+//int FindMaxSchedule (const std::vector<std::pair<int,int>>& intervals)
+//or 
+//int FindMaxSchedule(std::vector<std::pair<int, int>> intervals)
+
+int FindMaxSchedule (std::vector<std::pair<int,int>> intervals)
+{  
+   //your code goes here
+
+   return -1;
+}
+
