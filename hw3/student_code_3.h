3f90ae8092d555f550ac27897bb70fd21bd3cd4a
katherinees <katherinesteiner2019@u.northwestern.edu>
2019-01-26 18:17:22 -0600

    uploading files for hw3

---
+++ hw3/student_code_3.h
@@ -0,0 +1,42 @@
+///////////////////////////////////////////////////////////////////////////////
+// You need to 
+//    1. Read Problem 2 in homework assignment #3. 
+//    2. Implement function GetStudentName.
+//    3. Implement function FindMaxSchedule.
+//    4. Compile your code as explained in the PDF file.
+//    5. Run the executable.
+//    6. Test and debug your code.
+//    7. Submit your code ("student_code_3.h") and results ("solution_3.dat") 
+//       via Canvas.
+///////////////////////////////////////////////////////////////////////////////
+
+//required libraries
+#include <string>
+#include <vector> 
+
+//you can include standard C++ libraries here
+
+
+// This function should return your name.
+// The name should match your name in Canvas
+
+void GetStudentName(std::string& your_name)
+{
+   //replace the placeholders "Firstname" and "Lastname"
+   //with you first name and last name 
+   your_name.assign("Firstname Lastname");
+}
+
+
+//your goal is to implement the following function
+
+double FindBestAssignment (std::vector<double> a,std::vector<double> b,
+                           std::vector<double> r)
+
+{  
+    double max_revenue = 0;
+   
+    //you code goes here
+
+    return max_revenue;
+}
