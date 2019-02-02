1ececaa66378873413f3114ff653d620217ddaea
katherinees <katherinesteiner2019@u.northwestern.edu>
2019-02-02 15:52:21 -0600

    adding files for hw4

---
+++ hw4/student_code_4.h
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////////////////////////
+// You need to 
+//    1. Read Problem 1 in homework assignment #4. 
+//    2. Implement function GetStudentName.
+//    3. Implement functions ProblemA and ProblemB.
+//    4. Compile your code as explained in the PDF file.
+//    5. Run the executable.
+//    6. Test and debug your code.
+//    7. Submit your code ("student_code_4.h") and results ("solution_4.dat") 
+//       via Canvas.
+///////////////////////////////////////////////////////////////////////////////
+
+//required libraries
+#include <string>
+#include <vector> 
+
+//you can include standard C++ libraries here
+
+
+// This function should return your name.
+// The name should match your name in Canvas
+
+void GetStudentName(std::string& your_name)
+{
+   //replace the placeholders "Firstname" and "Lastname"
+   //with you first name and last name 
+   your_name.assign("Firstname Lastname");
+}
+
+int ProblemA (std::vector<int> X)
+{
+
+// ... your code ...
+
+   return -1; /* your answer */
+}
+
+int ProblemB (std::vector<int> X)
+{  
+
+   // ... your code ...
+
+   return -2; /* your answer */
+}
