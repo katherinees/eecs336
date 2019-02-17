67230a516f8b040e88e3277884cd194a8364a4e5
katherinees <katherinesteiner2019@u.northwestern.edu>
2019-02-16 18:08:12 -0600

    adding files for hw5

---
+++ hw5/student_code_5.h
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////////////////////////
+// You need to 
+//    1. Read Problem 2 in homework assignment #5. 
+//    2. Implement function GetStudentName.
+//    3. Implement function FindMaxProfit.
+//    4. Compile your code as explained in the PDF file.
+//    5. Run the executable.
+//    6. Test and debug your code.
+//    7. Submit your code ("student_code_5.h") and results ("solution_5.dat") 
+//       via Canvas.
+///////////////////////////////////////////////////////////////////////////////
+
+//required libraries
+#include <string>
+#include <vector> 
+
+//you can include standard C++ libraries here
+
+// This function should return your name.
+// The name should match your name in Canvas
+
+void GetStudentName(std::string& your_name)
+{
+   //replace the placeholders "Firstname" and "Lastname"
+   //with you first name and last name 
+   your_name.assign("Firstname Lastname");
+}
+
+
+//you can use one of the following signatures
+
+//int FindMaxProfit (const std::vector<int>& north, const std::vector<int>& west)
+//or 
+//int FindMaxProfit (std::vector<int> north, std::vector<int> west)
+
+int FindMaxProfit (const std::vector<int>& north, const std::vector<int>& west)
+{  
+   
+   //your code
+
+   return -1;
+}
+
