8f6d0fa5a26f60abc72043c5fc959242125faec4
katherinees <katherinesteiner2019@u.northwestern.edu>
2019-02-18 17:42:02 -0600

    wow oh rly helped

--- hw5/my_tests.cpp
+++ hw5/my_tests.cpp
@@ -57,156 +57,59 @@
 //                      3,  4   5,  6   7   8   9   a
 int FindMaxProfitTest (const std::vector<int>& north, const std::vector<int>& west)
 {
-  std::vector<int> n;
-  for (int k = 0; k < north.size(); k++) {
-    n.push_back(north[k]);
-  }
-  std::vector<int> w;
-  for (int i = 0; i < west.size(); i++) {
-    w.push_back(west[i]);
-  }
-  std::vector<int> subs;
-  std::vector<int> last;
-  if (n[0] > w[0]) {
-    subs.push_back(n[0]);
-    last.push_back(0);
-  } else {
-    subs.push_back(w[0]);
-    last.push_back(1);
-  }
-  if (n.size() == 1) {
-    return subs.back();
-  }
-  if (n[0]+n[1] > w[0]+w[1]) {
-    subs.push_back(n[0]+n[1]);
-    last.push_back(0);
-  } else {
-    subs.push_back(w[0]+w[1]);
-    last.push_back(1);
-  }
-  if (n.size() == 2) {
-    return subs.back();
-  }
-  int alln = n[0]+n[1]+n[2];
-  int allw = w[0]+w[1]+w[2];
-  int nw = n[0]+w[2];
-  int wn = w[0]+n[2];
-  if (alln > allw and alln > nw and alln > wn) {
-    subs.push_back(alln);
-    last.push_back(0);
-  }
-  if (allw > alln and allw > nw and allw > wn) {
-    subs.push_back(allw);
-    last.push_back(1);
-  }
-  if (nw > alln and nw > allw and nw > wn) {
-    subs.push_back(nw);
-    last.push_back(1);
-  }
-  if (wn > alln and wn > allw and wn > nw) {
-    subs.push_back(wn);
-    last.push_back(0);
-  }
-  if (n.size() == 3) {
-    return subs.back();
-  }
-  n.push_back(0);
-  w.push_back(0);
-  for (int i = 3; i < n.size()-1; i++) {
-    int swap;
-    int swapPrep = last[i-2];
-    if (swapPrep == 0) {
-      swap = subs[i-2]+w[i]+w[i+1];
-      swapPrep = 1;
-    } else {
-      swap = subs[i-2]+n[i]+n[i+1];
-      swapPrep = 0;
-    }
-    int keep;
-    int keepPrep = last[i-1];
-    if (keepPrep == 0) {
-      keep = subs[i-1]+n[i]+n[i+1];
-      keepPrep = 0;
-    } else {
-      keep = subs[i-1]+w[i]+w[i+1];
-      keepPrep = 1;
-    }
-    int undo;
-    int undoPrep = last[i-3];
-    if (undoPrep == 0) {
-      undo = subs[i-3]+n[i-2]+n[i-1]+n[i]+n[i+1];
-      undoPrep = 0;
-    } else {
-      undo = subs[i-3]+w[i-2]+w[i-1]+w[i]+w[i+1];
-      undoPrep = 1;
-    }
-    std::cout << "i = " << i << "\nswap = " << swap << "\nkeep = " << keep << "\nundo = " << undo << "\n";
-    if (swap > keep and swap > undo) {
-      if (swapPrep == 0) {
-        subs.push_back(swap - n[i+1]);
-      } else {
-        subs.push_back(swap - w[i+1]);
-      }
-      last.push_back(swapPrep);
-    } else if (keep > swap and keep > undo) {
-      if (keepPrep == 0) {
-        subs.push_back(keep - n[i+1]);
-      } else {
-        subs.push_back(keep - w[i+1]);
-      }
-      last.push_back(keepPrep);
-    } else {
-      if (undoPrep == 0) {
-        subs.push_back(undo - n[i+1]);
-      } else {
-        subs.push_back(undo - w[i+1]);
-      }
-      last.push_back(undoPrep);
+  std::vector<int> PN;
+  std::vector<int> PW;
+  std::vector<int> PL;
+  PN.push_back(north[0]);
+  PW.push_back(west[0]);
+  PL.push_back(0);
+  if (north.size() > 1) {
+    for (int i = 0; i < north.size()-1; i++) {
+      // std::cout << north[i+1] + std::max(PN[i], PL[i]) << "\n";
+      PN.push_back(north[i+1] + std::max(PN[i], PL[i]));
+      PW.push_back(west[i+1] + std::max(PW[i], PL[i]));
+      PL.push_back(std::max(PN[i], PW[i]));
     }
   }
-  for (int k = 0; k < subs.size(); k++) {
-    std::cout << subs[k] << ' ';
-  }
-  std::cout << "\n";
-  for (int k = 0; k < last.size(); k++) {
-    std::cout << last[k] << ' ';
-  }
-  std::cout << "\n";
 
-   //your code
-
-   return subs.back();
+  return std::max(PN.back(), PW.back());
 }
 
 int main() {
   std::vector<int> n;
   n.push_back(1);
-  n.push_back(1);
-  n.push_back(1);
-  n.push_back(105);
-  n.push_back(1);
-  n.push_back(107);
-  n.push_back(104);
-  n.push_back(107);
-  n.push_back(93);
-  n.push_back(92);
-  n.push_back(113);
-
+  n.push_back(95);
+  n.push_back(70);
   std::vector<int> w;
-  w.push_back(109);
   w.push_back(1);
-  w.push_back(111);
-  w.push_back(84);
-  w.push_back(79);
-  w.push_back(105);
-  w.push_back(1);
-  w.push_back(105);
-  w.push_back(1);
-  w.push_back(96);
-  w.push_back(1);
+  w.push_back(94);
+  w.push_back(102);
+  // std::vector<int> n;
+  // n.push_back(1);
+  // n.push_back(1);
+  // n.push_back(1);
+  // n.push_back(105);
+  // n.push_back(1);
+  // n.push_back(107);
+  // n.push_back(104);
+  // n.push_back(107);
+  // n.push_back(93);
+  // n.push_back(92);
+  // n.push_back(113);
+  //
+  // std::vector<int> w;
+  // w.push_back(109);
+  // w.push_back(1);
+  // w.push_back(111);
+  // w.push_back(84);
+  // w.push_back(79);
+  // w.push_back(105);
+  // w.push_back(1);
+  // w.push_back(105);
+  // w.push_back(1);
+  // w.push_back(96);
+  // w.push_back(1);
 
 
-
-
-  std::cout << "FUCKKK" << FindMaxProfitTest(n,w);
+  std::cout << "FUCKKK\n" << FindMaxProfitTest(n,w);
 }
