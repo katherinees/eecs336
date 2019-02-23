df12ea27941abadd146cd5f3685d32c7ce82a1f0
katherinees <katherinesteiner2019@u.northwestern.edu>
2019-02-23 16:26:27 -0600

    71 mistakes, im gonna change everything

--- hw6/my_tests.cpp
+++ hw6/my_tests.cpp
@@ -23,11 +23,71 @@ int FindMonotonePrediction (const std::vector<int>& y, int M)
   // std::cout << "\nFUCK\n";
   int best = M*M*y.size();
   int start_index = 0;
-  for (int m = 1; m <= M; m ++) {
+  for (int m = 0; m <= M; m ++) {
+    std::cout << "\ngoing forwards with start " << start_index << " and m " << m << "\n";
+
     for (int i = start_index; i < y.size(); i++) {
       if (i != 0) {
         if (y[i] < y[i-1]) {
-          x[i] = m;
+          x[i] = std::min(m, x[i-1]);
+        } else {
+          x[i] = std::min(m, y[i]);
+          x[i] = std::max(x[i], x[i-1]);
+        }
+      } else {
+        x[i] = std::min(m, y[i]);
+      }
+    }
+    for (int j = 0; j < y.size(); j++) {
+      std::cout << x[j] << " ";
+    }
+    int attempt = ComputeCost(y, x);
+    std::cout << "-- " << attempt << "\nFUCK\n";
+    best = std::min(best, attempt);
+
+    m++;
+    if (m>M) { break; }
+    int save = 0;
+    int b = y.size();
+    int keep[b];
+    for (int k = 0; k < y.size(); k++) {
+      keep[k] = x[k];
+    }
+
+    int so_far = M*M*y.size();
+    for (int j = y.size(); j >= 0; j--) {
+      x[j] = m;
+      int maybe = ComputeCost(y, x);
+      if (maybe < so_far) {
+        so_far = maybe;
+        save = j;
+      }
+    }
+
+    std::cout << "\nSAVE IS " << save << "\n";
+
+    for (int l = 0; l < save; l++) {
+      x[l] = keep[l];
+    }
+
+    for (int j = 0; j < y.size(); j++) {
+      std::cout << x[j] << " ";
+    }
+    attempt = ComputeCost(y, x);
+    std::cout << "-- " << attempt << "\nFUCK\n";
+    best = std::min(best, attempt);
+
+    start_index = save;
+  }
+
+  start_index = 0;
+  for (int m = 1; m <= M; m ++) {
+    std::cout << "\ngoing forwards with start " << start_index << " and m " << m << "\n";
+
+    for (int i = start_index; i < y.size(); i++) {
+      if (i != 0) {
+        if (y[i] < y[i-1]) {
+          x[i] = std::min(m, x[i-1]);
         } else {
           x[i] = std::min(m, y[i]);
           x[i] = std::max(x[i], x[i-1]);
@@ -86,15 +146,15 @@ int FindMonotonePrediction (const std::vector<int>& y, int M)
 int main() {
   std::vector<int> y;
   y.push_back(0);
-  y.push_back(0);
-  y.push_back(4);
-  y.push_back(0);
-  y.push_back(0);
   y.push_back(2);
-  y.push_back(2);
-  y.push_back(7);
+  y.push_back(0);
+  y.push_back(0);
   y.push_back(6);
   y.push_back(0);
+  y.push_back(1);
+  y.push_back(4);
+  y.push_back(5);
+  y.push_back(8);
 
   //
   // int test [5] = {0};
