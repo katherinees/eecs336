03e941a599292c5452442ec1a3d92e9efa231f44
katherinees <katherinesteiner2019@u.northwestern.edu>
2019-02-22 18:49:09 -0600

    adding new files for hw6

---
+++ hw6/student_code_6.h
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////////////
+// You need to 
+//    1. Read Problem 2 in homework assignment #6. 
+//    2. Implement function GetStudentName.
+//    3. Implement function FindMonotonePrediction.
+//    4. Compile your code as explained in the PDF file.
+//    5. Run the executable.
+//    6. Test and debug your code.
+//    7. Submit your code ("student_code_5.h") and results ("solution_5.dat") 
+//       via Canvas.
+///////////////////////////////////////////////////////////////////////////////
+
+//required libraries
+#include <string>
+#include <vector> 
+
+//you can include standard C++ libraries here
+#include <queue>
+
+// This function should return your name.
+// The name should match your name in Canvas
+
+void GetStudentName(std::string& your_name)
+{
+   //replace the placeholders "Firstname" and "Lastname"
+   //with you first name and last name 
+   your_name.assign("Firstname Lastname");
+}
+
+int FindMonotonePrediction (const std::vector<int>& y, int M)
+{  
+
+   /* code */
+
+   return -1;
+}
